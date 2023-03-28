b2a323b465e367420153ec0068b832f75d419940
LorenSklar <LorenSklar@outlook.com>
2023-03-28 12:23:18 -0400

    Expands Player class and adds Square class
    This version of games expands the Player class to include show and skill attributes where show is True if the board is displayed after that player moves and skill is the skill level of the player from 0 to 1.
    
    Also, implements a Square class to store information about row, column, and player index for a given square on the board.
    
    Also, removes the extra row and column from the board previously used to hold row and column label information. 
    
    Row and column are again limited to a single digit at least for now

--- games.py
+++ games.py
@@ -1,155 +1,93 @@
-from config import *
-from math import log
-
 class Board():
-	def __init__(self, rows = 8, columns = 0):
+  def __init__ (self, row_count = 8, column_count = 0):
+    self.row_count = row_count
 
-		# set number of rows
-		self.rows = rows
+    if column_count > 0:
+      self.column_count = column_count
 
-		# set row height
-		self.row_height = 1
+    else:
+      self.column_count = row_count
 
-		# set number of columns
-		if columns > 0:
-			self.columns = columns
+    assert row_count < 10
+    assert column_count < 10
 
-		else:
-			self.columns = rows
+    self.position = [[0 for col in range(self.column_count)] for row in range(self.row_count)]
 
-		self.column_width = int(log(self.columns - 1, 26) + 1)
+    self.pieces = []
+  
+  def display(self):
+    # print column labels
+    self.print_line(None, self.get_empty_label,  self.get_column_label, "   ")
+    for r in range(self.column_count):
+      # print row boarder
+      self.print_line(r, self.get_empty_label, self.get_dash, " + ")
+      # print row
+      self.print_line(r, self. get_row_label, self.get_cell, " | ")
+    # print final row boarder
+    self.print_line(r, self.get_empty_label, self.get_dash, " + ")
+    # repeat column labels
+    self.print_line(None, self.get_empty_label,  self.get_column_label, "   ")
 
-		# insert empty cells
-		self.data = [[0 for c in range(self.columns + 1)] for r in range(self.rows + 1)]
+  def get_column_label(self, r, c):
+    number = ord("A") + c
+    label = chr(number)
+    return label
+  
+  def get_cell(self, r, c):
+    number = self.position[r][c]
+    piece = self.pieces[number]
+    return piece
+  
+  def get_dash(self, r, c):
+    return "-"
+    
+  def get_empty_label(self, r):
+    return " "
+    
+  def get_row_label(self, r):
+    label = str(r + 1)
+    return label  
 
-	# convert column number to letter
-	def num2let(self, number):
-		alpha = ""
-		number -= 1
-		while number >= 0:
-			digit = ord("A")
-			digit += number % 26 
+  def parse(self, move):
+    row_label = ""
+    col_label = ""
+    for letter in move:
+      if letter.isnumeric():
+        row_label += letter
 
-			alpha = chr(digit) + alpha
+      if letter.isalpha():
+        col_label += letter
 
-			number /= 26
-			number = int(number)
-			number -= 1
+    row = int(row_label) - 1
+    col = ord(col_label.upper()) - ord("A")
 
-		return alpha
+    return row, col
+    
+  def print_line(self, r, label, content, separator):
+    line = " "
+    line += label(r)
+    for c in range(self.column_count):
+      line += separator
+      line += content(r, c)
+    line += separator
+    line += label(r)
+    print(line)
+  
+  def update(self, row, col, player_index):
+    self.position[row][col] = player_index
 
-	# construct column label
-	def get_column_label(self, r, c):
-		# get label
-		alpha = self.num2let(c)
-		
-		# how wide is column?
-		width = self.column_width
-
-		# center label in column
-		width -= len(alpha)
-		right = width // 2
-		left = width - right
-		label = " " * left + alpha + " " * right
-
-		return label
-
-	# construct row label
-	def get_row_label(self, r):
-		# maximum width of label?
-		big = str(self.rows)
-
-		# this label?
-		alpha = str(self.rows - r + 1)
-
-		# difference?
-		spaces = len(big) - len(alpha)
-
-		# construct row label
-		label = " " * spaces + alpha
-
-		return label
-
-	# construct empty row label
-	def get_empty_label(self, r):
-		# determine 
-		width = len(str(self.rows))
-
-		# construct row label
-		label = " " * width
-
-		return label
-
-	# construct a single cell using board data
-	def get_cell(self, r, c):
-
-		# column width?
-		width = self.column_width
-		
-		# find symbol
-		i = self.data[r][c]
-		alpha = SYMBOLS[i]
-
-		# center label in column
-		width -= len(alpha)
-		right = width // 2
-		left = width - right
-
-		cell = " " * left + alpha + " " * right
-
-		return cell
-
-	# construct boarder for a single cell
-	def get_boarder(self, r, c):
-		# column width?
-		width = self.column_width
-
-		return "-" * width
-
-	# construct a full line for display
-	def print_line(self, r, label, content, seperator):
-		# start single space
-		line = " "
-
-		# add row label
-		line += label(r)
-
-		# add each cell
-		for c in range(1, self.columns + 1):
-			line += seperator
-			line += content(r, c)
-
-		# add final separator
-		line += seperator
-
-		# repeat row label
-		line += label(r)
-
-		# add final space
-		line += " "
-
-		print(line)
-
-	# display entire board
-	def display(self):
-		# start with column labels
-		self.print_line(None, self.get_empty_label, self.get_column_label, "   ")
-
-		# print each row
-		for r in range(1, self.rows + 1):
-			self.print_line(None, self.get_empty_label, self.get_boarder, " + ")
-			self.print_line(r, self.get_row_label, self.get_cell, " | ")
-
-		# print final boarder
-		self.print_line(None, self.get_empty_label, self.get_boarder, " + ")
-
-		# repeat column labels
-		self.print_line(None, self.get_empty_label, self.get_column_label, "   ")
-
+class Square():
+  def __init__ (self, row, col, val):
+    self.r = row
+    self.c = col
+    self.i = val
 
 class Player():
-  def __init__(self, name, symbol, go):
+  def __init__ (self, name, symbol, action, show = False, skill = 0):
     self.name = name
     self.symbol = symbol
-    self.go = go
+    self.go = action
+    self.show = show
+    self.skill = skill
 
+    
