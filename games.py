2be5b3947ce5f4a9ac71b2a9c4b952d605ff5bb5
LorenSklar <LorenSklar@outlook.com>
2023-03-29 15:09:34 -0400

    Construct Board using Square class
    Board.position is now a list of squares using the fully implemented Square class
    
    Also adds board.rows, board.columns, and board.diagonals to hold a list of valid rows, etc for each board

--- games.py
+++ games.py
@@ -1,3 +1,15 @@
+class Square():
+  def __init__(self, value = 0):
+    self.value = value
+
+class Player():
+  def __init__ (self, name, symbol, action, visible = False, skill = 0):
+    self.name = name
+    self.symbol = symbol
+    self.go = action
+    self.show = visible
+    self.skill = skill
+    
 class Board():
   def __init__ (self, row_count = 8, column_count = 0):
     self.row_count = row_count
@@ -11,83 +23,49 @@ class Board():
     assert row_count < 10
     assert column_count < 10
 
-    self.position = [[0 for col in range(self.column_count)] for row in range(self.row_count)]
+    self.position = []
 
-    self.pieces = []
-  
+    for r in range(1, self.row_count + 1):
+      for c in range(1, self.column_count + 1):
+        self.position.append(Square())
+
+    self.rows = []
+    for r in range(1, self.row_count + 1):
+      row = []
+      for c in range(1, self.column_count + 1):
+        row.append((r, c))
+      self.rows.append(row)
+
+    self.columns = []
+    for c in range(1, self.column_count + 1):
+      column = []
+      for r in range(1, self.row_count + 1):
+        column.append((r, c))
+      self.columns.append(column)
+
+    diagonal = []
+    antidiagonal = []
+    for r in range(1, self.row_count + 1):
+      for c in range(1, self.column_count + 1):
+
+        if r == c:
+          diagonal.append((r, c))
+
+        if r == self.column_count - c + 1:
+          antidiagonal.append((r, c))
+
+    self.diagonals = [diagonal]
+    self.antidiagonals = [antidiagonal]
+    
   def display(self):
-    # print column labels
-    self.print_line(None, self.get_empty_label,  self.get_column_label, "   ")
-    for r in range(self.column_count):
-      # print row boarder
-      self.print_line(r, self.get_empty_label, self.get_dash, " + ")
-      # print row
-      self.print_line(r, self. get_row_label, self.get_cell, " | ")
-    # print final row boarder
-    self.print_line(r, self.get_empty_label, self.get_dash, " + ")
-    # repeat column labels
-    self.print_line(None, self.get_empty_label,  self.get_column_label, "   ")
+    for i, square in enumerate(self.position):
+      if (i + 1) % self.column_count == 0:
+        end = "\n"
+      else:
+        end = " "
+      
+      print(square.value, end = end)
 
-  def get_column_label(self, r, c):
-    number = ord("A") + c
-    label = chr(number)
-    return label
-  
-  def get_cell(self, r, c):
-    number = self.position[r][c]
-    piece = self.pieces[number]
-    return piece
-  
-  def get_dash(self, r, c):
-    return "-"
-    
-  def get_empty_label(self, r):
-    return " "
-    
-  def get_row_label(self, r):
-    label = str(r + 1)
-    return label  
-
-  def parse(self, move):
-    row_label = ""
-    col_label = ""
-    for letter in move:
-      if letter.isnumeric():
-        row_label += letter
-
-      if letter.isalpha():
-        col_label += letter
-
-    row = int(row_label) - 1
-    col = ord(col_label.upper()) - ord("A")
-
-    return row, col
-    
-  def print_line(self, r, label, content, separator):
-    line = " "
-    line += label(r)
-    for c in range(self.column_count):
-      line += separator
-      line += content(r, c)
-    line += separator
-    line += label(r)
-    print(line)
-  
-  def update(self, row, col, player_index):
-    self.position[row][col] = player_index
-
-class Square():
-  def __init__ (self, row, col, val):
-    self.r = row
-    self.c = col
-    self.i = val
-
-class Player():
-  def __init__ (self, name, symbol, action, show = False, skill = 0):
-    self.name = name
-    self.symbol = symbol
-    self.go = action
-    self.show = show
-    self.skill = skill
-
-    
+  def update(self, r, c, player_index):
+    i = (r - 1) * self.column_count + (c - 1)
+    self.position[i].value = player_index
