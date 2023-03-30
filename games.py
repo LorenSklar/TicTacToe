b9216f4d5c6c3ccb633f8bac547f812f89d8cd5f
LorenSklar <LorenSklar@outlook.com>
2023-03-30 14:20:50 -0400

    Removes Square Class
    The only information stored in the Square class was the piece on the square so I reversed my decision on creating a Square class. The position information for the board is now stored as a list of integer values.

--- games.py
+++ games.py
@@ -1,9 +1,5 @@
-class Square():
-  def __init__(self, value = 0):
-    self.value = value
-
 class Player():
-  def __init__ (self, name, symbol, action, visible = False, skill = 0):
+  def __init__(self, name, symbol, action, visible = True, skill = 0):
     self.name = name
     self.symbol = symbol
     self.go = action
@@ -22,12 +18,6 @@ class Board():
 
     assert row_count < 10
     assert column_count < 10
-
-    self.position = []
-
-    for r in range(1, self.row_count + 1):
-      for c in range(1, self.column_count + 1):
-        self.position.append(Square())
 
     self.rows = []
     for r in range(1, self.row_count + 1):
@@ -54,18 +44,39 @@ class Board():
         if r == self.column_count - c + 1:
           antidiagonal.append((r, c))
 
-    self.diagonals = [diagonal]
-    self.antidiagonals = [antidiagonal]
+    self.diagonals = [diagonal, antidiagonal]
+
+    self.values = [0 for i in range(self.row_count * self.column_count)]
+
+    self.symbol_list = []
+    
+  def copy(self):
+    out = Board(self.row_count, self.column_count)
+    out.values = self.values.copy()
+    out.symbol_list = self.symbol_list.copy()
+    return out
     
   def display(self):
-    for i, square in enumerate(self.position):
+    for i in range(len(self.values)):
+      index = self.values[i]
+      symbol = self.symbol_list[index]
+
       if (i + 1) % self.column_count == 0:
         end = "\n"
       else:
         end = " "
-      
-      print(square.value, end = end)
+        
+      print(symbol, end = end)
 
+  def select(self, r, c):
+    out = []
+    rows = self.rows + self.columns + self.diagonals
+    for row in rows:
+      if (r, c) in row:
+        out.append(row)
+    return out
+    
   def update(self, r, c, player_index):
     i = (r - 1) * self.column_count + (c - 1)
-    self.position[i].value = player_index
+    self.values[i] = player_index
+   
